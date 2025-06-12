90b32fca8c1824a12d45595372df727c1cdbf2c7
Karina <karinaayres@hotmail.com>
2025-06-12 19:46:26 -0300

    Questão 2

--- RESOLUCAOP2/todos/views.py
+++ RESOLUCAOP2/todos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from .models import Todo
+
+class TodoListView(ListView):
+    model = Todo
+
+
